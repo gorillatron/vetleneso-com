799d9998b31bcae9b86c62a4afc0ff1b16292dfe
gorillatron <jornandretangen@gmail.com>
2015-11-13 20:40:52 +0530

    werk

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,31 @@
 import React, {Component, PropTypes} from "react"
 import {connect} from "react-redux"
+import { Router, Route, Link } from 'react-router'
+
+
+class Site extends Component {
+
+  render() {
+    return (
+      <div>
+        Site
+        {this.props.children}
+      </div>
+    )
+  }
+
+}
 
 
 class App extends Component {
 
   render() {
-    return <div>app container</div>
+    return (
+      <Router>
+        <Route path="/" component={Site}>
+        </Route>
+      </Router>
+    )
   }
 
 }
