1431d30c16f343079adfa6df1a98430e25f727be
gorillatron <jornandretangen@gmail.com>
2015-11-19 18:24:05 +0530

    werk

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,60 @@
 
 import React, {Component, PropTypes} from "react"
+import Radium from "radium"
 
-class About extends Component {
+var About = Radium(class extends Component {
 
   render() {
     return (
       <div className="row">
 
-        <div className="two columns">&nbsp;</div>
+        <div className="one column">&nbsp;</div>
 
-        <div className="eight columns"></div>
+        <div className="ten columns">
+          <img src="/images/selvportrett.jpg"
+               style={{
+                 width: 200,
+                 float: 'left',
+                 margin: '0px 20px 20px 0px'
+               }}/>
 
-        <div className="two columns">&nbsp;</div>
+              <p>Lorem ipsum dolor sit amet, eu epicurei appareat eam. Vel te alienum iracundia. Vix ex tractatos iudicabit, in solet numquam fuisset ius, tantas decore exerci sit ut. Ne saepe deleniti invenire mei, eu mei sint aeque euismod.</p>
+
+              <p>Ei odio prima usu. Pri id tacimates inciderint ullamcorper, commune quaestio voluptatibus ei quo. Mei saepe expetendis scribentur in, pro quas cotidieque ut. Usu nonumes consulatu instructior in.</p>
+
+              <p>An nibh recteque gloriatur mel, vim nostro scaevola ea. Enim clita accommodare vis at. An eos feugait fastidii petentium, qualisque consectetuer ad vim. Assueverit dissentiunt et eos.</p>
+
+              <p>Dictas euripidis at eam, possit inermis no mea. Cum id etiam malorum. In has aliquid accusam, per putent intellegam id, primis nostrud democritum in sed. Id tritani comprehensam eos, id wisi mundi viris his, fugit dictas animal ex qui. Essent tractatos repudiandae est et, eu eos vitae voluptatum.</p>
+
+              <p>Ea iisque insolens mea, eos ea sumo modus suscipit. Ne mel iisque hendrerit, amet deserunt has te, vix mazim everti delicatissimi in. Pri laudem labore id, summo adipisci quaestio et eum, cu vix possim doctus definitiones. Tamquam definitiones ex pri, in pro legimus posidonium omittantur.</p>
+
+                <p>Lorem ipsum dolor sit amet, eu epicurei appareat eam. Vel te alienum iracundia. Vix ex tractatos iudicabit, in solet numquam fuisset ius, tantas decore exerci sit ut. Ne saepe deleniti invenire mei, eu mei sint aeque euismod.</p>
+
+                <p>Ei odio prima usu. Pri id tacimates inciderint ullamcorper, commune quaestio voluptatibus ei quo. Mei saepe expetendis scribentur in, pro quas cotidieque ut. Usu nonumes consulatu instructior in.</p>
+
+                <p>An nibh recteque gloriatur mel, vim nostro scaevola ea. Enim clita accommodare vis at. An eos feugait fastidii petentium, qualisque consectetuer ad vim. Assueverit dissentiunt et eos.</p>
+
+                <p>Dictas euripidis at eam, possit inermis no mea. Cum id etiam malorum. In has aliquid accusam, per putent intellegam id, primis nostrud democritum in sed. Id tritani comprehensam eos, id wisi mundi viris his, fugit dictas animal ex qui. Essent tractatos repudiandae est et, eu eos vitae voluptatum.</p>
+
+                <p>Ea iisque insolens mea, eos ea sumo modus suscipit. Ne mel iisque hendrerit, amet deserunt has te, vix mazim everti delicatissimi in. Pri laudem labore id, summo adipisci quaestio et eum, cu vix possim doctus definitiones. Tamquam definitiones ex pri, in pro legimus posidonium omittantur.</p>
+
+                  <p>Lorem ipsum dolor sit amet, eu epicurei appareat eam. Vel te alienum iracundia. Vix ex tractatos iudicabit, in solet numquam fuisset ius, tantas decore exerci sit ut. Ne saepe deleniti invenire mei, eu mei sint aeque euismod.</p>
+
+                  <p>Ei odio prima usu. Pri id tacimates inciderint ullamcorper, commune quaestio voluptatibus ei quo. Mei saepe expetendis scribentur in, pro quas cotidieque ut. Usu nonumes consulatu instructior in.</p>
+
+                  <p>An nibh recteque gloriatur mel, vim nostro scaevola ea. Enim clita accommodare vis at. An eos feugait fastidii petentium, qualisque consectetuer ad vim. Assueverit dissentiunt et eos.</p>
+
+                  <p>Dictas euripidis at eam, possit inermis no mea. Cum id etiam malorum. In has aliquid accusam, per putent intellegam id, primis nostrud democritum in sed. Id tritani comprehensam eos, id wisi mundi viris his, fugit dictas animal ex qui. Essent tractatos repudiandae est et, eu eos vitae voluptatum.</p>
+
+                  <p>Ea iisque insolens mea, eos ea sumo modus suscipit. Ne mel iisque hendrerit, amet deserunt has te, vix mazim everti delicatissimi in. Pri laudem labore id, summo adipisci quaestio et eum, cu vix possim doctus definitiones. Tamquam definitiones ex pri, in pro legimus posidonium omittantur.</p>
+        </div>
+
+        <div className="one column">&nbsp;</div>
 
       </div>
     )
   }
 
-}
+})
 
 export default About
--- src/containers/App.js
+++ src/containers/App.js
@@ -13,13 +13,27 @@ const links = [
 var Header = Radium(class extends Component {
 
   render() {
+
+    const imageHeight = this.props.imageHeight
+    const backgroundOpacity = imageHeight < 250 ? 0.96 : 0
+
     return (
-      <header>
+      <header className="row"
+              ref="header"
+              style={{ position: 'fixed',
+                       transition: 'all 0.33s ease',
+                       backgroundColor: "rgba(255,255,255, " +backgroundOpacity+ ")",
+                       boxShadow: imageHeight < 120 ? '0px 2px 0px 0px rgba(0,0,0,1)' : 'none',
+                       top: 0,
+                       width: 'inherit',
+                       maxWidth: '960px',
+                       display: 'block'}}>
 
         <div id="logo-wrapper"
              className="twelve column"
              style={{
-               height: 300
+               transition: 'height 0.15s ease',
+               height: imageHeight
              }}>
           <img src="/images/neso_logo@1x.png"
                style={{
@@ -33,7 +47,8 @@ var Header = Radium(class extends Component {
           <ul style={{
             textAlign: 'center',
             fontSize: '1.55em',
-            listStyleType: 'none'
+            listStyleType: 'none',
+            margin: 0
           }}>
             {links.map((link, index) => (
               <li style={{ display: 'inline-block',
@@ -64,31 +79,63 @@ var Header = Radium(class extends Component {
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      scrollTop: 0,
+      lastScrollTop: 0
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const lastScrollTop = this.state.scrollTop
+      const scrollTop = document.querySelector('body').scrollTop
+      this.setState({ lastScrollTop, scrollTop })
+    }, false)
+  }
+
   render() {
+
+    const scrollBase = this.state.scrollTop >= 0 ? (200 - (this.state.scrollTop)) : 0
+    const headerImageHeight =  (scrollBase > 0 ? scrollBase : 0) + 72.3
+
     return (
-      <div style={{
-        width: '100%',
-        height: '100%',
-        'backgroundImage': 'url("/images/chickentown_lr.jpg")',
-        'backgroundSize': 'cover'
-      }}>
+      <div style={{ width: '100%',
+                    height: '100%'}}>
 
-        <div className="outer-wrapper container"
-             style={{
-               padding: '20px',
-               minHeight: '100%',
-               backgroundColor: "rgba(255,255,255, 0.97)"
-             }}>
-
-          <div className="row">
-            <Header/>
-          </div>
-
-          <div id="content">
-            {this.props.children}
-          </div>
-
+        <div style={{ position: 'fixed',
+                      zIndex: 1,
+                      width: '100%',
+                      height: '100%',
+                      'backgroundImage': 'url("/images/chickentown_lr.jpg")',
+                      'backgroundSize': 'cover' }}>
         </div>
+
+        <div style={{ position: 'relative',
+                      zIndex: 2,
+                      width: '100%',
+                      height: '100%'}}>
+
+            <div className="container"
+                 style={{ padding: '1px',
+                          minHeight: '100%',
+                          backgroundColor: "rgba(255,255,255, 0.97)"
+                 }}>
+
+              <Header imageHeight={headerImageHeight}/>
+
+              <div id="content"
+                   ref="outer-wrapper"
+                   style={{ transition: 'margin-top 0.15s ease',
+                            minHeight: '100%',
+                            marginTop: headerImageHeight + 75 }}>
+                {this.props.children}
+              </div>
+
+            </div>
+
+          </div>
 
       </div>
     )
