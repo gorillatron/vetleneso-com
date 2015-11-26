80e538fce52f250cd5bcc8e620fc28409b0d6467
gorillatron <jornandretangen@gmail.com>
2015-11-26 11:09:28 +0530

    werk

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,11 @@ var babelrc = JSON.parse(fs.readFileSync('./.babelrc', 'utf8'))
 if(!babelrc.plugins)
   babelrc.plugins = []
 
-
+// Expose api/client as module 'api' to the server
 babelrc.plugins.push(["babel-plugin-module-alias", [
   { "src": "./src/api/client.js", "expose": "api" }
 ]])
 
-
-console.log(babelrc)
 
 gulp.task("build-client", (cb) => {
   browserify({ debug: true })
@@ -36,34 +34,6 @@ gulp.task("build-client", (cb) => {
       })
       cb() })
     .pipe(fs.createWriteStream("./resources/js/client.js"))
-    .on("finish", () => {
-      notify({
-        type: 'pass',
-        title: 'Gulp',
-        subtitle: 'Build success',
-        message: "success",
-        group: 'taskdoer'
-      })
-      cb()
-    })
-})
-
-gulp.task("build-server", (cb) => {
-  browserify({ debug: true })
-    .transform(babelify)
-    .require("./src/server.js", { entry: true })
-    .bundle()
-    .on("error", (err) => {
-      console.log("Error: " + err.message)
-      notify({
-        type: 'fail',
-        title: 'Gulp',
-        subtitle: 'Build fail',
-        message: err.message,
-        group: 'taskdoer'
-      })
-      cb() })
-    .pipe(fs.createWriteStream("./dist/server.js"))
     .on("finish", () => {
       notify({
         type: 'pass',
--- startserver.js
+++ startserver.js
@@ -6,6 +6,7 @@ var babelrc = JSON.parse(fs.readFileSync('./.babelrc', 'utf8'))
 if(!babelrc.plugins)
   babelrc.plugins = []
 
+// Expose api/server as module 'api' to the server
 babelrc.plugins.push(["babel-plugin-module-alias", [
   { "src": "./src/api/server.js", "expose": "api" }
 ]])
