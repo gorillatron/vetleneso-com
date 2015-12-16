fbedba823a77b3e8f91332bab4eb87c5e52072b4
gorillatron <jornandretangen@gmail.com>
2015-12-16 11:44:24 +0530

    favicons

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -4,9 +4,45 @@ import { connect } from 'react-redux'
 import splitArray from 'split-array'
 import { Link } from 'react-router'
 import { fetchGallery, setSelectedImage } from '../actions'
+import Radium from 'radium'
 
 
-class Gallery extends Component {
+class FullScreenImage extends Component {
+
+  render() {
+    return (
+      <div ref="element"
+           style={{ position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    zIndex: 10,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'rgba(0,0,0, 0.8)' }}>
+        <Link to="/gallery">
+          <div style={{ position: 'absolute',
+                        cursor: 'pointer',
+                        top: '15px',
+                        right: '20px',
+                        fontSize: '1.7em',
+                        color: 'white'}}>
+            &#10006;
+          </div>
+        </Link>
+        <img ref="selected-image-img"
+             src={this.props.selectedImage.imageUrl}
+             style={{ display: 'block',
+                      margin: '5% auto',
+                      maxWidth: '80%',
+                      height: '80%'}}/>
+      </div>
+    )
+  }
+
+}
+
+
+const Gallery = Radium(class extends Component {
 
   static query = [
     fetchGallery
@@ -54,49 +90,29 @@ class Gallery extends Component {
     return (
       <div>
 
-        {selectedImage &&
-          <div style={{ position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        zIndex: 10,
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: 'rgba(0,0,0, 0.8)' }}>
-            <Link to="/gallery">
-              <div style={{ position: 'absolute',
-                            cursor: 'pointer',
-                            top: '15px',
-                            right: '20px',
-                            fontSize: '1.7em',
-                            color: 'white'}}>
-                &#10006;
-              </div>
-            </Link>
-            <img ref="selected-image-img"
-                 src={selectedImage.imageUrl}
-                 style={{ display: 'block',
-                          margin: '5% auto',
-                          maxWidth: '80%',
-                          height: '80%'}}/>
-          </div>}
+        { selectedImage &&
+          <FullScreenImage selectedImage={selectedImage} /> }
 
-        {imageRows.map((row, index) => (
+        {imageRows.map((row, rowindex) => (
 
-          <div key={`row_${index}`}
+          <div key={`row_${rowindex}`}
                className="row"
                style={{ marginBottom: '15px' }}>
 
-            {row.map((image, index) => (
+            {row.map((image, imageindex) => (
 
-              <div key={`galleryimage_${index}`}
+              <div key={`galleryimage_${rowindex}/${imageindex}`}
                    className="three columns">
 
                 <Link to={`/gallery/${image.fileName}`}>
-                  <img className="gallery-image"
-                       style={{ width: '100%',
-                                opacity: 0.01,
-                                cursor: 'pointer',
-                                transform: 'scale(0.01)' }}
+                  <img ref={`${image.fileName}_img`}
+                       className="gallery-image"
+                       style={{
+                         width: '100%',
+                         opacity: 0.01,
+                         cursor: 'pointer',
+                         transform: 'scale(0.01)'
+                       }}
                        src={image.thumbUrl} />
                 </Link>
 
@@ -109,7 +125,7 @@ class Gallery extends Component {
       </div>
     )
   }
-}
+})
 
 
 function select(state) {
